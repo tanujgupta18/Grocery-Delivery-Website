790318b6c3b2773557cfbdf026af4b9ff259c148
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-05-05 17:38:41 +0530

    Navbar

---
+++ client/src/assets/assets.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import search_icon from "./search_icon.svg";
+import nav_cart_icon from "./nav_cart_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import profile_icon from "./profile_icon.png";
+
+export const assets = {
+  logo,
+  search_icon,
+  nav_cart_icon,
+  menu_icon,
+  profile_icon,
+};
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,6 +8,7 @@ export const AppContextProvider = ({ children }) => {
 
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
+  const [showUserLogin, setShowUserLogin] = useState(false);
 
   const value = {
     navigate,
@@ -15,6 +16,8 @@ export const AppContextProvider = ({ children }) => {
     setUser,
     isSeller,
     setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
