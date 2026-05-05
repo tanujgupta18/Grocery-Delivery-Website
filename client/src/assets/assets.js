0289938fb50653cfa7bfb2dcc919bc78ac88aa0a
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-05-05 18:57:34 +0530

    Main Banner

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -3,6 +3,10 @@ import search_icon from "./search_icon.svg";
 import nav_cart_icon from "./nav_cart_icon.svg";
 import menu_icon from "./menu_icon.svg";
 import profile_icon from "./profile_icon.png";
+import main_banner_bg from "./main_banner_bg.png";
+import main_banner_bg_sm from "./main_banner_bg_sm.png";
+import white_arrow_icon from "./white_arrow_icon.svg";
+import black_arrow_icon from "./black_arrow_icon.svg";
 
 export const assets = {
   logo,
@@ -10,4 +14,8 @@ export const assets = {
   nav_cart_icon,
   menu_icon,
   profile_icon,
+  main_banner_bg,
+  main_banner_bg_sm,
+  white_arrow_icon,
+  black_arrow_icon,
 };
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MainBanner from "../components/MainBanner";
+
+export const Home = () => {
+  return (
+    <div className="mt-10">
+      <MainBanner />
+    </div>
+  );
+};
