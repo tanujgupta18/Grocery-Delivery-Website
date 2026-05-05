644f853512c6458c1abd98ffc0a936a5206f449f
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-05-05 19:30:52 +0530

    Categories Section

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -7,6 +7,13 @@ import main_banner_bg from "./main_banner_bg.png";
 import main_banner_bg_sm from "./main_banner_bg_sm.png";
 import white_arrow_icon from "./white_arrow_icon.svg";
 import black_arrow_icon from "./black_arrow_icon.svg";
+import organic_vegitable_image from "./organic_vegitable_image.png";
+import fresh_fruits_image from "./fresh_fruits_image.png";
+import bottles_image from "./bottles_image.png";
+import maggi_image from "./maggi_image.png";
+import dairy_product_image from "./dairy_product_image.png";
+import bakery_image from "./bakery_image.png";
+import grain_image from "./grain_image.png";
 
 export const assets = {
   logo,
@@ -19,3 +26,48 @@ export const assets = {
   white_arrow_icon,
   black_arrow_icon,
 };
+
+export const categories = [
+  {
+    text: "Organic veggies",
+    path: "Vegetables",
+    image: organic_vegitable_image,
+    bgColor: "#FEF6DA",
+  },
+  {
+    text: "Fresh Fruits",
+    path: "Fruits",
+    image: fresh_fruits_image,
+    bgColor: "#FEE0E0",
+  },
+  {
+    text: "Cold Drinks",
+    path: "Drinks",
+    image: bottles_image,
+    bgColor: "#F0F5DE",
+  },
+  {
+    text: "Instant Food",
+    path: "Instant",
+    image: maggi_image,
+    bgColor: "#E1F5EC",
+  },
+  {
+    text: "Dairy Products",
+    path: "Dairy",
+    image: dairy_product_image,
+    bgColor: "#FEE6CD",
+  },
+  {
+    text: "Bakery & Breads",
+    path: "Bakery",
+    image: bakery_image,
+    bgColor: "#E0F6FE",
+  },
+  {
+    text: "Grains & Cereals",
+    path: "Grains",
+    image: grain_image,
+    bgColor: "#F1E3F9",
+  },
+];
