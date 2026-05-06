36e5e815fa7061cb949a6e41d7ef8c9b17cd7b18
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-05-06 10:15:05 +0530

    Footer Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {isSellerPath ? null : <Footer />}
     </>
   );
 };
